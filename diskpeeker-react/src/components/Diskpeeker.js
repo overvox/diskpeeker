51d0c258fa10926be26644986b5dcb0253903e63
overvox <13432493+overvox@users.noreply.github.com>
2023-01-28 17:11:31 +0100

    removed unused imports

--- diskpeeker-react/src/components/Diskpeeker.js
+++ diskpeeker-react/src/components/Diskpeeker.js
@@ -1,7 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import axios from "axios"
 import DiskUsageBar from './DiskUsageBar';
-import DiskApi from '../App'
 
 function bytesToGigaBytes(bytes) {
   return (bytes / 1_073_741_824).toFixed(0);
