f689f0c55a5a2703da9b80ac299319945968e261
overvox <13432493+overvox@users.noreply.github.com>
2023-02-09 23:12:53 +0100

    Added dev/prod settings differentiation

--- diskpeeker-django/diskpeeker_project/diskpeeker_project/settings.py
+++ diskpeeker-django/diskpeeker_project/diskpeeker_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,13 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7tpp$l87i24axg(+1c6u^2xm&g_7@=9)%zq9x!a8c_i3j*3)-k'
+SECRET_KEY = os.getenv('DISKPEEKER_API_SECRET')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1'] # this is the host that Docker uses to run application
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'debussy']
 
 
 # Application definition
@@ -122,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -131,6 +132,10 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # react js front end cors whitelisting 
 CORS_ALLOWED_ORIGINS = [
-     'http://localhost:3080'
+     'http://localhost:3080',
      'http://debussy:3080'
 ]
+
+# Override production variables if DJANGO_DEVELOPMENT env variable is true
+if os.getenv('DJANGO_DEVELOPMENT') == 'true':
+    from .settings_dev import *  # or specific overrides
